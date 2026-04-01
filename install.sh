c06b23d466e0503a38794cfb905bf97e7a8cf5c9
Jose Hernandez <98195541+hernandezj1@users.noreply.github.com>
2026-04-01 09:51:41 -0500

    Update download link and extraction for ollama

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 
 srun -A backfill2 -n 4 -N 1 --gres=gpu:4500:1 --pty /bin/bash
 
-curl -L https://github.com/ollama/ollama/releases/download/v0.13.5/ollama-windows-arm64.zip -o ollama-linux-arm64.tgz
+curl -L https://github.com/ollama/ollama/releases/download/v0.13.5/ollama-linux-amd64.zip -o ollama-linux-amd64.tgz
 
 mkdir -p ollama
 
-tar -C ollama -xvzf ollama-linux-arm64.tgz
+tar -C ollama -xvzf ollama-linux-amd64.tgz
 
 ./ollama/bin/ollama --version
 
